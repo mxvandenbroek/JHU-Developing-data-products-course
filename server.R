c937075fadf0f6a16f331fb8bdff8465b366121e
mxvandenbroek <m.x.vandenbroek@gmail.com>
2015-10-24 17:58:10 +0200

    Third attempt

--- server.R
+++ server.R
@@ -41,24 +41,3 @@ shinyServer(function(input, output) {
 })
 
 
-# shinyServer(function(input, output) {
-#         
-#         # Compute the formula text in a reactive expression since it is
-#         # shared by the output$caption and output$mpgPlot functions
-#         formulaText <- reactive({
-#                 paste("mpg ~", input$variable)
-#         })
-#         
-#         # Return the formula text for printing as a caption
-#         output$caption <- renderText({
-#                 formulaText()
-#         })
-#         
-#         # Generate a plot of the requested variable against mpg and
-#         # only include outliers if requested
-#         output$mpgPlot <- renderPlot({
-#                 boxplot(as.formula(formulaText()),
-#                         data = mpgData,
-#                         outline = input$outliers)
-#         })
-# })
